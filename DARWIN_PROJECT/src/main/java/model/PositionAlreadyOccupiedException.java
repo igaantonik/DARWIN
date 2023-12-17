ee91e7a4c056e9e4d9056b737ef995b387724d33
igaantonik <igaantonik@student.agh.edu.pl>
2023-12-17 01:53:48 +0100

    Revert "Revert "dodanie klas, interfejsow i zamiany w plant, water""
    This reverts commit 60e4f855ab80e63381ee07a47f0ed9dcf59226cd.

---
+++ DARWIN_PROJECT/src/main/java/model/PositionAlreadyOccupiedException.java
@@ -0,0 +1,9 @@
+package model;
+
+public class PositionAlreadyOccupiedException extends Exception{
+    public PositionAlreadyOccupiedException(Vector2d position) {
+
+        super("Position " + position + " is already occupied.");
+
+    }
+}
