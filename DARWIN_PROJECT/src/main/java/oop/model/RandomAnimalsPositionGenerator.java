5e91b2201dc592ff62ef66e80d67937f3f9fb0d7
hytrek <hytrek@student.agh.edu.pl>
2024-01-15 02:29:09 +0100

    Scalowanie mapy i inne cuda niewidy

--- DARWIN_PROJECT/src/main/java/oop/model/RandomAnimalsPositionGenerator.java
+++ DARWIN_PROJECT/src/main/java/oop/model/RandomAnimalsPositionGenerator.java
@@ -1,9 +1,6 @@
 package oop.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class RandomAnimalsPositionGenerator implements Iterable, Iterator<Vector2d> {
     private List<Vector2d> positions;
@@ -11,14 +8,15 @@ public class RandomAnimalsPositionGenerator implements Iterable, Iterator<Vector
     private int positions_len;
 
     public RandomAnimalsPositionGenerator(int maxWidth, int maxHeight, int animalCount){
+        Random rand = new Random();
         this.current_position = 0;
         this.positions_len = animalCount;
         ArrayList<Integer> list_x = new ArrayList<Integer>();
-        for (int i=0; i < maxWidth; i++) list_x.add(i);
+        for (int i=0; i < animalCount; i++) list_x.add(rand.nextInt(0,maxWidth-1));
         Collections.shuffle(list_x);
 
         ArrayList<Integer> list_y = new ArrayList<Integer>();
-        for (int i=0; i < maxHeight; i++) list_y.add(i);
+        for (int i=0; i < animalCount; i++) list_y.add(rand.nextInt(0,maxHeight-1));
         Collections.shuffle(list_y);
 
         List<Vector2d> positions = new ArrayList<>();
